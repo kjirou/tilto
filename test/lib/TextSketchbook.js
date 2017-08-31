8708ad83586a776c6078138f0a76bd187f7af34c
kjirou <sorenariblog@gmail.com>
2017-09-01 00:26:21 +0900

    Add `addBox`

--- test/lib/TextSketchbook.js
+++ test/lib/TextSketchbook.js
@@ -1,9 +1,25 @@
 const assert = require('assert');
 
+const Box = require('../../lib/Box');
 const TextSketchbook = require('../../lib/TextSketchbook');
 
 
 describe('lib/TextSketchbook', function() {
+  describe('addBox', function() {
+    it('should throw an error when boxId is duplicated', function() {
+      const book = new TextSketchbook(1, 2);
+      const box1 = new Box({x: 0, y: 0, width: 1, height: 1}, null, 'a');
+      const box2 = new Box({x: 0, y: 0, width: 1, height: 1}, null, 'b');
+      const box3 = new Box({x: 0, y: 0, width: 1, height: 1}, null, 'a');
+
+      book.addBox(box1);
+      book.addBox(box2);
+      assert.throws(() => {
+        book.addBox(box3);
+      }, /duplicated/);
+    });
+  });
+
   describe('drawBox', function() {
     it('works', function() {
       const book = new TextSketchbook(3, 4, {symbol: 'A'});
