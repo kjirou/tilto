27a7dff17ad9ce727abf528aeb623938d186454d
kjirou <sorenariblog@gmail.com>
2017-09-16 15:02:00 +0900

    Redefine `_cropMatrix` as a static method

--- test/lib/Box.js
+++ test/lib/Box.js
@@ -41,45 +41,44 @@ describe('lib/Box', function() {
     });
   });
 
-  describe('instance methods', function() {
-    describe('_cropMatrix', function() {
-      const box = Box.fromText([
-        '123',
-        '456',
-        '789',
-        'abc',
-      ].join('\n'));
+  describe('_cropMatrix', function() {
+    const box = Box.fromText([
+      '123',
+      '456',
+      '789',
+      'abc',
+    ].join('\n'));
+    const baseMatrix = box.getMatrix();
 
-      it('can crop the inside', function() {
-        const matrix1 = box._cropMatrix({x: 0, y: 0, width: 2, height: 3});
-        assert.strictEqual(matrix1[0][0].symbol, '1');
-        assert.strictEqual(matrix1[0][1].symbol, '2');
-        assert.strictEqual(matrix1[1][0].symbol, '4');
-        assert.strictEqual(matrix1[1][1].symbol, '5');
-        assert.strictEqual(matrix1[2][0].symbol, '7');
-        assert.strictEqual(matrix1[2][1].symbol, '8');
+    it('can crop the inside', function() {
+      const matrix1 = Box._cropMatrix(baseMatrix, {x: 0, y: 0, width: 2, height: 3});
+      assert.strictEqual(matrix1[0][0].symbol, '1');
+      assert.strictEqual(matrix1[0][1].symbol, '2');
+      assert.strictEqual(matrix1[1][0].symbol, '4');
+      assert.strictEqual(matrix1[1][1].symbol, '5');
+      assert.strictEqual(matrix1[2][0].symbol, '7');
+      assert.strictEqual(matrix1[2][1].symbol, '8');
 
-        const matrix2 = box._cropMatrix({x: 1, y: 2, width: 2, height: 2});
-        assert.strictEqual(matrix2[0][0].symbol, '8');
-        assert.strictEqual(matrix2[0][1].symbol, '9');
-        assert.strictEqual(matrix2[1][0].symbol, 'b');
-        assert.strictEqual(matrix2[1][1].symbol, 'c');
-      });
+      const matrix2 = Box._cropMatrix(baseMatrix, {x: 1, y: 2, width: 2, height: 2});
+      assert.strictEqual(matrix2[0][0].symbol, '8');
+      assert.strictEqual(matrix2[0][1].symbol, '9');
+      assert.strictEqual(matrix2[1][0].symbol, 'b');
+      assert.strictEqual(matrix2[1][1].symbol, 'c');
+    });
 
-      it('should ignore the outside part', function() {
-        const matrix1 = box._cropMatrix({x: 2, y: 0, width: 2, height: 2});
-        assert.strictEqual(matrix1[0][0].symbol, '3');
-        assert.strictEqual(matrix1[1][0].symbol, '6');
+    it('should ignore the outside part', function() {
+      const matrix1 = Box._cropMatrix(baseMatrix, {x: 2, y: 0, width: 2, height: 2});
+      assert.strictEqual(matrix1[0][0].symbol, '3');
+      assert.strictEqual(matrix1[1][0].symbol, '6');
 
-        const matrix2 = box._cropMatrix({x: 0, y: 3, width: 2, height: 2});
-        assert.strictEqual(matrix2[0][0].symbol, 'a');
-        assert.strictEqual(matrix2[0][1].symbol, 'b');
-      });
+      const matrix2 = Box._cropMatrix(baseMatrix, {x: 0, y: 3, width: 2, height: 2});
+      assert.strictEqual(matrix2[0][0].symbol, 'a');
+      assert.strictEqual(matrix2[0][1].symbol, 'b');
+    });
 
-      it('should return null if it can not crop at all', function() {
-        const matrix = box._cropMatrix({x: 3, y: 0, width: 2, height: 3});
-        assert.strictEqual(matrix, null);
-      });
+    it('should return null if it can not crop at all', function() {
+      const matrix = Box._cropMatrix(baseMatrix, {x: 3, y: 0, width: 2, height: 3});
+      assert.strictEqual(matrix, null);
     });
   });
 });
