485550667edc93686e87df17cd0c7fc2d66389b5
kjirou <sorenariblog@gmail.com>
2017-09-17 13:36:35 +0900

    Add a method

--- test/lib/Box.js
+++ test/lib/Box.js
@@ -82,6 +82,44 @@ describe('lib/Box', function() {
     });
   });
 
+  describe('_shrinkRectangle', function() {
+    const rect = {x: 1, y: 2, width: 3, height: 4};
+
+    it('can apply no paddings', function() {
+      assert.deepStrictEqual(Box._shrinkRectangle(rect), {x: 1, y: 2, width: 3, height: 4});
+    });
+
+    it('can pad to the top', function() {
+      assert.deepStrictEqual(Box._shrinkRectangle(rect, {top: 2}), {x: 1, y: 4, width: 3, height: 2});
+    });
+
+    it('can set to the bottom', function() {
+      assert.deepStrictEqual(Box._shrinkRectangle(rect, {bottom: 2}), {x: 1, y: 2, width: 3, height: 2});
+    });
+
+    it('can pad to the left', function() {
+      assert.deepStrictEqual(Box._shrinkRectangle(rect, {left: 2}), {x: 3, y: 2, width: 1, height: 4});
+    });
+
+    it('can pad to the right', function() {
+      assert.deepStrictEqual(Box._shrinkRectangle(rect, {right: 2}), {x: 1, y: 2, width: 1, height: 4});
+    });
+
+    it('can set to all sides', function() {
+      assert.deepStrictEqual(
+        Box._shrinkRectangle(rect, {top: 1, bottom: 1, left: 1, right: 1}),
+        {x: 2, y: 3, width: 1, height: 2}
+      );
+    });
+
+    it('can be set the size to 0', function() {
+      assert.deepStrictEqual(
+        Box._shrinkRectangle(rect, {top: 1, bottom: 3, left: 1, right: 2}),
+        {x: 2, y: 3, width: 0, height: 0}
+      );
+    });
+  });
+
   describe('instance methods', function() {
     describe('asString', function() {
       describe('content pouring', function() {
