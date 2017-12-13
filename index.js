d84d8b655d1a9aaac1cb74beedd70683ab639af9
kjirou <sorenariblog@gmail.com>
2017-12-14 02:37:52 +0900

    Define public apis

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-module.exports = {};
+// @flow
+
+const boxUtils = require('./lib/box-utils');
+const matrixUtils = require('./lib/matrix-utils');
+
+/*::
+import type {Box} from './lib/box-utils';
+import type {Coordinate} from './lib/coordinate-utils';
+import type {
+  Element,
+  Matrix,
+  SymbolRuler,
+} from './lib/matrix-utils';
+import type {Rectangle} from './lib/rectangle-utils';
+import type {Size} from './lib/size-utils';
+
+export type {
+  Box,
+  Coordinate,
+  Element,
+  Matrix,
+  Rectangle,
+  Size,
+  SymbolRuler,
+};
+ */
+
+
+function getWidth(box/*: Box*/)/*: number*/ {
+  return matrixUtils.getWidth(box.matrix);
+}
+
+function getHeight(box/*: Box*/)/*: number*/ {
+  return matrixUtils.getHeight(box.matrix);
+}
+
+function getMaxX(box/*: Box*/)/*: number*/ {
+  return matrixUtils.getMaxX(box.matrix);
+}
+
+function getMaxY(box/*: Box*/)/*: number*/ {
+  return matrixUtils.getMaxY(box.matrix);
+}
+
+module.exports = {
+  getMaxX,
+  getMaxY,
+  getHeight,
+  getWidth,
+  initializeBox: boxUtils.initializeBox,
+  initializeBoxFromText: boxUtils.initializeBoxFromText,
+  initializeMatrix: matrixUtils.initializeMatrix,
+  initializeMatrixFromText: matrixUtils.initializeMatrixFromText,
+  render: boxUtils.toText,
+  setBorders: boxUtils.setBorders,
+};
