7906ee66311308fda80430b58372dc595c1f1a4d
kjirou <sorenariblog@gmail.com>
2017-12-14 04:36:33 +0900

    Add an example

---
+++ examples/dummy-notepad.js
@@ -0,0 +1,48 @@
+const tilto = require('../index');
+
+const logUpdate = require('log-update');
+const keypress = require('keypress');
+
+
+let root = tilto.createBox({width: 40, height: 20});
+
+let statusBar = tilto.createBox({width: 40, height: 3});
+statusBar = tilto.setBorderType(statusBar, 'default');
+statusBar.content = '[C]lear | [Q]uit';
+
+let textarea = tilto.createBox({y: 3, width: 40, height: 17});
+textarea = tilto.setBorders(textarea, {
+  bottomWidth: 1,
+  leftWidth: 1,
+  rightWidth: 1,
+  bottomSymbols: ['-'],
+  leftSymbols: ['|'],
+  rightSymbols: ['|'],
+  bottomLeftSymbols: ['+'],
+  bottomRightSymbols: ['+'],
+});
+
+root.children.push(statusBar, textarea);
+
+
+keypress(process.stdin);
+process.stdin.setRawMode(true);
+
+process.stdin.on('keypress', (ch, key) => {
+  if (key && key.ctrl && (key.name === 'd' || key.name === 'q')) {
+    process.stdin.pause();
+    process.exit();
+  } else if (key && key.ctrl && key.name === 'c') {
+    textarea.content = '';
+    logUpdate(tilto.render(root));
+  } else {
+    let character = ch;
+    if (key && key.name === 'return') {
+      character = '\n';
+    }
+    textarea.content += character;
+    logUpdate(tilto.render(root));
+  }
+});
+
+logUpdate(tilto.render(root));
--- index.js
+++ index.js
@@ -14,7 +14,10 @@ import type {
 import type {Rectangle} from './lib/rectangle-utils';
 import type {Size} from './lib/size-utils';
 
+type BorderType = 'default';
+
 export type {
+  BorderType,
   Box,
   Coordinate,
   Element,
@@ -42,6 +45,29 @@ function getMaxY(box/*: Box*/)/*: number*/ {
   return matrixUtils.getMaxY(box.matrix);
 }
 
+function setBorderType(box/*: Box*/, borderType/*: BorderType*/)/*: Box*/ {
+  switch (borderType) {
+  case 'default':
+    return Object.assign({}, box, {
+      borders: {
+        topWidth: 1,
+        bottomWidth: 1,
+        leftWidth: 1,
+        rightWidth: 1,
+        topSymbols: ['-'],
+        bottomSymbols: ['-'],
+        leftSymbols: ['|'],
+        rightSymbols: ['|'],
+        topLeftSymbols: ['+'],
+        topRightSymbols: ['+'],
+        bottomLeftSymbols: ['+'],
+        bottomRightSymbols: ['+'],
+      },
+    });
+  }
+  return box;
+}
+
 module.exports = {
   createBox: boxUtils.createBox,
   createBoxFromText: boxUtils.createBoxFromText,
@@ -53,4 +79,5 @@ module.exports = {
   getWidth,
   render: boxUtils.toText,
   setBorders: boxUtils.setBorders,
+  setBorderType,
 };
