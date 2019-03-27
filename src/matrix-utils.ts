23db53f00bcaf0117d4ced8857b73f0cdb656038
kjirou <sorenariblog@gmail.com>
2019-03-27 09:31:10 +0900

    Move the `decodeAnsiStyles` into the "matrix-utils.js"
    Because it returns almost the same data type of `ElementStyle`.

--- src/matrix-utils.ts
+++ src/matrix-utils.ts
@@ -1,6 +1,9 @@
+import * as ansiRegex from 'ansi-regex';
+
 import {
   Coordinate,
   Size,
+  stringToArray,
 } from './utils';
 import {
   Rectangle,
@@ -9,6 +12,7 @@ import {
   toSize,
 } from './rectangle-utils';
 
+const ansiStyles = require('ansi-styles');
 const sliceAnsiString = require('slice-ansi-string');
 const stripAnsi = require('strip-ansi');
 
@@ -281,6 +285,137 @@ export function cropMatrix(matrix: Matrix, rectangle: Rectangle): Matrix {
   }
 
   return newMatrix;
+}
+
+/**
+ * @param character A single character with or without ANSI escape codes.
+ *                  It is mainly assumed to pass the return value of "slice-ansi-string".
+ */
+export function decodeAnsiStyles(character: string): {
+  foregroundColor: string | void,
+  backgroundColor: string | void,
+  bold: boolean,
+  dim: boolean,
+  italic: boolean,
+  underline: boolean,
+  inverse: boolean,
+  hidden: boolean,
+  strikethrough: boolean,
+} {
+  const characterWithoutAnsi = stripAnsi(character);
+  if (stringToArray(characterWithoutAnsi).length !== 1) {
+    throw new Error('It should be used for single characters.');
+  }
+
+  const ansiEscapeCodes = character.match(ansiRegex()) || [];
+
+  // From https://github.com/chalk/ansi-styles#colors
+  const foregroundColorNames = [
+    'black',
+    'red',
+    'green',
+    'yellow',
+    'blue',
+    'magenta',
+    'cyan',
+    'white',
+    // NOTE: "blackBright" will be added in the following commit.
+    //       https://github.com/chalk/ansi-styles/commit/fb5b656d9fce745881c36deb8ff800b5080d9f89
+    'grey',
+    //'blackBright',
+    'redBright',
+    'greenBright',
+    'yellowBright',
+    'blueBright',
+    'magentaBright',
+    'cyanBright',
+    'whiteBright',
+  ];
+  let foregroundColor = undefined;
+  ansiEscapeCodes.some(code => {
+    const foundName = foregroundColorNames.find(name => {
+      return code === (ansiStyles.color[name] && ansiStyles.color[name].open);
+    });
+    if (foundName) {
+      foregroundColor = foundName;
+      return true;
+    }
+    return false;
+  });
+
+  // From https://github.com/chalk/ansi-styles#background-colors
+  const backgroundColorNames = [
+    'bgBlack',
+    'bgRed',
+    'bgGreen',
+    'bgYellow',
+    'bgBlue',
+    'bgMagenta',
+    'bgCyan',
+    'bgWhite',
+    'bgBlackBright',
+    'bgRedBright',
+    'bgGreenBright',
+    'bgYellowBright',
+    'bgBlueBright',
+    'bgMagentaBright',
+    'bgCyanBright',
+    'bgWhiteBright',
+  ];
+  let backgroundColor = undefined;
+  ansiEscapeCodes.some(code => {
+    const foundName = backgroundColorNames.find(name => {
+      return code === (ansiStyles.bgColor[name] && ansiStyles.bgColor[name].open);
+    });
+    if (foundName) {
+      backgroundColor = foundName;
+      return true;
+    }
+    return false;
+  });
+
+  // From) https://github.com/chalk/ansi-styles#modifiers
+  const modifiers = {
+    bold: false,
+    dim: false,
+    italic: false,
+    underline: false,
+    inverse: false,
+    hidden: false,
+    strikethrough: false,
+  };
+  ansiEscapeCodes.forEach(code => {
+    if (code === ansiStyles.bold.open) {
+      modifiers.bold = true;
+    } else if (code === ansiStyles.dim.open) {
+      modifiers.dim = true;
+    } else if (code === ansiStyles.italic.open) {
+      modifiers.italic = true;
+    } else if (code === ansiStyles.underline.open) {
+      modifiers.underline = true;
+    } else if (code === ansiStyles.inverse.open) {
+      modifiers.inverse = true;
+    } else if (code === ansiStyles.hidden.open) {
+      modifiers.hidden = true;
+    } else if (code === ansiStyles.strikethrough.open) {
+      modifiers.strikethrough = true;
+    }
+  });
+
+  return Object.assign(
+    {
+      foregroundColor,
+      backgroundColor,
+      bold: false,
+      dim: false,
+      italic: false,
+      underline: false,
+      inverse: false,
+      hidden: false,
+      strikethrough: false,
+    },
+    modifiers
+  );
 }
 
 export function parseContent(content: string): {
